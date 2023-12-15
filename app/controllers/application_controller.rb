6446e972f5423227ef46e5d36c17b6ff52bb4653
amina.saif0 <amina.saif@pakeventures.com>
2023-12-15 11:12:16 +0500

    Routes removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     protect_from_forgery with: :exception
-
-    def after_sign_in_path_for(resource)
-        if resource.is_a?(User)
-            case resource.roles
-            when 'admin'
-                articles_path
-            when 'user'
-                articles_path
-            else
-                articles_path
-            end
-        else
-            super
-        end
-    end
 end
